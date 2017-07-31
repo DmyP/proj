6160e6370e8ec3a4870becc264d1ce60eff9f684
DmyP <Pechenyi.D@gmail.com>
2017-07-31 18:23:18 +0300

    Add UserServlet

---
+++ src/main/java/com/ep/proj/web/UserServlet.java
@@ -0,0 +1,24 @@
+package com.ep.proj.web;
+
+import com.ep.proj.controller.UserController;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UserServlet extends HttpServlet {
+    private UserController userController;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("users", userController.getAll());
+        request.getRequestDispatcher("/users.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
